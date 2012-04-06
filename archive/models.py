f690d2b34267aa39d152a741cc5c3af9a1f0a920
hitzler <hitze.is.heat@gmail.com>
2012-04-06 12:42:21 -0400

    #3 testin

--- archive/models.py
+++ archive/models.py
@@ -3,8 +3,16 @@ from django.db import models
 class Series(models.Model):
     title  = models.CharField(max_length=255)
     slug   = models.SlugField(max_length=255)
-    #Aliases IDK M8
+    # Aliases
     genres = models.ManyToManyField('Genre')
+    # Start Date
+    # Series Status
+    # Series Original Network
+    # Series Air Day
+    # Series Air Time
+    # Series Country
+    # Series Season Count
+    # Series Locked (Y/N)
 
 class Genre(models.Model):
     title = models.CharField(max_length=70)
