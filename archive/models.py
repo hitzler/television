41112415bf5bdd83729e48865cdcf9bf36bd1422
hitzler <hitze.is.heat@gmail.com>
2012-04-06 15:12:45 -0400

    #3 testin

--- archive/models.py
+++ archive/models.py
@@ -1,20 +1,30 @@
 from django.db import models
+DAY_CHOICES = (
+    ('MON', 'Monday'),
+    ('TUE', 'Tuesday'),
+    ('WED', 'Wednesday'),
+    ('THU', 'Thursday'),
+    ('FRI', 'Friday'),
+    ('SAT', 'Saturday'),
+    ('SUN', 'Sunday'),
+    ('WEK', 'Weekdays'),
+    ('DAL', 'Daily')
+)
 
 class Series(models.Model):
-    title  = models.CharField(max_length=255)
-    slug   = models.SlugField(max_length=255)
-    # Aliases
-    genres = models.ManyToManyField('Genre')
-
-    # Start Date
-    # Series Status
-    # Series Air Day
-    # Series Air Time
-    # Series Country
-    # Series Season Count
+    title   = models.CharField(max_length=255)
+    slug    = models.SlugField(max_length=255)
+    genres  = models.ManyToManyField('Genre')
+    airDay  = models.CharField(max_length=2, choices=DAY_CHOICES)
+    seasons = models.IntegerField()
     network = models.ForeignKey('Network')
     locked  = models.BooleanField(default=False)
-    locker  = models.ForeignKey('User')
+
+    #locker = models.ForeignKey('User')
+    # Start Date
+    # Series Status
+    # Series Air Time
+    # Series Country
 
 class Genre(models.Model):
     title = models.CharField(max_length=70)
