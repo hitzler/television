3cd3ad1a50e5370bdd2ea6411171d87c2d4c7541
hitzler <hitze.is.heat@gmail.com>
2012-04-07 02:13:31 -0400

    Doing thangs

---
+++ archive/admin.py
@@ -0,0 +1,27 @@
+from archive.models import *
+from django.contrib import admin
+
+class SeriesAdmin(admin.ModelAdmin):
+    list_display  = ['title', 'network']
+    search_fields = ['title']
+    prepopulated_fields =  {'slug': ['title']}
+
+class EpisodeAdmin(admin.ModelAdmin):
+    list_display  = ['title', 'series']
+    search_fields = ['title', 'series']
+    prepopulated_fields = {'slug': ['title']}
+
+class NetworkAdmin(admin.ModelAdmin):
+    list_display  = ['title']#, 'country']
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ['title']}
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+
+admin.site.register(Series, SeriesAdmin)
+admin.site.register(Episode, EpisodeAdmin)
+admin.site.register(Network, NetworkAdmin)
+admin.site.register(Genre, GenreAdmin)
